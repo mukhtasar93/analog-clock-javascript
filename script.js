ce34bf3f6b5018829d70d5503ad0b4957db52808
mukhtasar93 <mukhtasar03@gmail.com>
2023-03-03 11:58:35 +0700

    fix hour

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const updateTime = () => {
 	let date = new Date(),
 		secToDeg = (date.getSeconds() / 60) * 360,
 		minToDeg = (date.getMinutes() / 60) * 360,
-		hourToDeg = (date.getHours() / 12) * 360;
+		hourToDeg = date.getHours() / 24;
 
 	secondHand.style.transform = `rotate(${secToDeg}deg)`;
 	minuteHand.style.transform = `rotate(${minToDeg}deg)`;
